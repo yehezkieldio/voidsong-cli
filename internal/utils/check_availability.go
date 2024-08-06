a32b41e907ee2b154681f2b4465845c9dd79f917
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-06 16:36:56 +0800

    feat: consistent item

--- internal/utils/check_availability.go
+++ internal/utils/check_availability.go
@@ -9,14 +9,14 @@ import (
 func CheckAvailability() (bool, error) {
 	l := CheckPackageJSON()
 	if !l {
-		fmt.Println(ui.ErrorTextStyle.Render(" Can't find a package.json file in the current directory! Are you sure you're in the right place?"))
+		fmt.Println(ui.ErrorTextStyle.Render("- Can't find a package.json file in the current directory! Are you sure you're in the right place?"))
 		return true, nil
 	}
 	fmt.Println(ui.TextStyle.Render("- Found package.json file!"))
 
 	b := CheckBunProject()
 	if !b {
-		fmt.Println(ui.ErrorTextStyle.Render(" Currently I only support projects that use Bun! Please make sure you have Bun installed and using it in your project."))
+		fmt.Println(ui.ErrorTextStyle.Render("- Currently I only support projects that use Bun! Please make sure you have Bun installed and using it in your project."))
 		return true, nil
 	}
 	fmt.Println(ui.TextStyle.Render("- Found bun.lockb file!"))
